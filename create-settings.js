864bb8be7e69873f033f98e41fc7ec08749efa62
Emily <emil202821@hotmail.com>
2016-05-25 01:31:03 +0200

    Added script to create settings

---
+++ create-settings.js
@@ -0,0 +1,40 @@
+
+var settings = {};
+
+settings.userinfo = {
+	username: "",
+	password: "",
+	steamGuard: ""
+}
+
+settings.login = {
+	loginMode: 0,
+	state: "online",
+	playGame: "205"
+}
+
+settings.info = {
+	version: "1.0"
+}
+
+settings.chats = [];
+
+settings.commands = {
+	ver: true,
+	id: true,
+	chatid: true,
+	random: true,
+	lock: true,
+	unlock: true,
+	timeout: true,
+	users: true,
+	play: true,
+	rules: true,
+	setname: true,
+	kick: true,
+	updated: true,
+	add: true
+}
+
+console.log(settings);
+require('fs').writeFileSync('./settings.json', JSON.stringify(settings, null, '\t'));
